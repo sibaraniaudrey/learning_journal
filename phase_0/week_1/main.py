c02e3e381e44bcb393fa6c23b4a3483bc7df5dad
Audrey Dominique <sibaraniaudrey@gmail.com>
2022-04-17 15:44:52 +0200

    'd3am'

---
+++ phase_0/week_1/main.py
@@ -0,0 +1,39 @@
+import numpy as np
+from package import fisika, matematika
+print(np.log(10))
+
+# cara I
+# import fisika
+# print(fisika.kecepatan(100,60))
+# print(fisika.volume(10,10,10))
+
+# cara II
+# from fisika import kecepatan
+# print(kecepatan(300,60))
+
+# from matematika import *
+# print(tambah(19,10))
+# print(perkalian(10,10))
+
+# cara I
+# import package.fisika as fis
+# print(fis.kecepatan(1000,8))
+
+#cara II
+# from package.fisika import *
+# print(fis.kecepatan(1000,8))
+
+fisika
+print(fisika.kecepatan(1000,100))
+
+matematika
+print(matematika.perkalian(100,10))
+
+# cara I
+import numpy as np
+from numpy import log
+print(log(10))
+
+# cara II
+import numpy as np
+print(np.log(10))
---
+++ phase_0/week_1/package/fisika.py
@@ -0,0 +1,11 @@
+def volume (panjang,lebar,tinggi):
+    '''
+    ini adalah fungsi volume
+    '''
+    return panjang*lebar*tinggi
+
+def kecepatan (jarak,waktu):
+    '''
+    ini rumus kecepatan
+    '''
+    return jarak/waktu
